8bf5407a71459b4c4d4a4b1192e97b1f311a9764
Allan Njoroge <allcreativewrites@gmail.com>
2024-02-15 21:57:41 +0300

    feat: Created department resource, registered the api endpoints and added  CRUD operations to departments

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 from flask import Flask
 from flask_migrate import Migrate
@@ -7,6 +7,10 @@ from flask_migrate import Migrate
 from models import db
 
 from routes.employee_bp import employee_bp
+from routes.documents_bp import document_bp
+from routes.department_bp import department_bp
+from routes.education_bp import education_bp
+
 
 from dotenv import load_dotenv
 
@@ -16,12 +20,17 @@ import os
 def create_app():
     app = Flask(__name__)
     load_dotenv()
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'SQLALCHEMY_DATABASE_URI')
     migrate = Migrate(app, db)
     db.init_app(app)
 
     app.register_blueprint(employee_bp)
-
+    app.register_blueprint(department_bp)
+    app.register_blueprint(education_bp)
+    app.register_blueprint(document_bp)
 
     return app
-app=create_app()
+
+
+app = create_app()
--- server/serializer.py
+++ server/serializer.py
@@ -4,10 +4,10 @@ from flask_restful import Api, Resource, abort, reqparse
 from flask_bcrypt import Bcrypt
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-from models import db, Employee, Department, Documents, Experience, EmployeeTraining, EmployeeProfile, HR_Personel, HrProfile, Manager, ManagerProfile, Leave, LeaveApproval, Session, Training, Remuneration, RemunerationDescription, Goals
+from models import db, Employee, Department, Documents, Experience, EmployeeTraining, EmployeeProfile, HR_Personel, HrProfile, Manager, ManagerProfile, Leave, LeaveApproval, Session, Training, Remuneration, RemunerationDescription, Goals, Education
 
 serializer_bp = Blueprint('serializer_bp', __name__)
-ma=Marshmallow(serializer_bp)
+ma = Marshmallow(serializer_bp)
 bcrypt = Bcrypt()
 api = Api(serializer_bp)
 
@@ -16,4 +16,29 @@ class EmployeeSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Employee
 
+
 employeeSchema = EmployeeSchema()
+
+
+class DocumentSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Documents
+
+
+documentSchema = DocumentSchema()
+
+
+class EducationSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Education
+
+
+educationSchema = EducationSchema()
+
+
+class DepartmentSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Department
+
+
+departmentSchema = DepartmentSchema()
