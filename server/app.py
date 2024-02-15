5bba64a1999593ee740ebfdc46671a5e99072be9
KELVIN MURITHI <kelvinmurithi@live.com>
2024-02-16 00:58:54 +0300

    Add new blueprint routes to Flask app
    Add new blueprints for goals, sessions, training, employee training, and leave approval to Flask app

--- server/app.py
+++ server/app.py
@@ -7,6 +7,11 @@ from flask_migrate import Migrate
 from models import db
 
 from routes.employee_bp import employee_bp
+from routes.session_bp import session_bp
+from routes.Goals_bp import goals_bp
+from routes.training_bp import training_bp
+from routes.employee_training_bp import employee_training_bp
+from routes.leave_approval_bp import leave_approval_bp
 
 from dotenv import load_dotenv
 
@@ -21,6 +26,11 @@ def create_app():
     db.init_app(app)
 
     app.register_blueprint(employee_bp)
+    app.register_blueprint(session_bp)
+    app.register_blueprint(goals_bp)
+    app.register_blueprint(training_bp)
+    app.register_blueprint(employee_training_bp)
+    app.register_blueprint(leave_approval_bp)
 
 
     return app
