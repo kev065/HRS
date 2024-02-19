b3127e2e0219ff6eb498ea4736b4dfb0cef56b5d
+ <+>
2024-02-19 20:30:37 +0300

    feat: adminLTE intergration inserted adminLTE dashboard to HRS next: Edit adminLTE

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Components/Home';
-import Login from './Components/Login';
-import Register from './Components/Register';
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home';
+import SideNav from './components/SideNav';
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        Hello
+        <Header/>
+        <Home/>
+        <SideNav/>
+        <Footer/>
+        {/* <Routes>
+          <Route path="/home" element={<Header />} />
+          <Route path="/login" element={<Home/>} />
+          <Route path="/register" element={<SideNav />} />
+          <Route path="/footer" element={<Footer/>} />
+
+        </Routes> */}
       </div>
     </Router>
   );
