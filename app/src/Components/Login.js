04f717cec8c610e6a87ba8daf5b2bf3180bfef53
anisamuema <anisa.muema.school@gmail.com>
2024-02-19 17:12:24 +0300

    feat: complete login and register

--- app/src/Components/Login.js
+++ app/src/Components/Login.js
@@ -1,97 +1,67 @@
-import React, { useState } from 'react'
-import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
-import 'admin-lte/dist/css/adminlte.min.css';
-import { useNavigate} from 'react-router-dom';
-
+import React, {  useState } from 'react'
+import { NavLink, useNavigate} from 'react-router-dom';
 
 const Login = () => {
 
-  const navigate = useNavigate()  
+    const navigate = useNavigate()
 
-  const [email, setemail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Implement your login logic here
-    fetch('/login', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email, password
-        })
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        // Redirect to the dashboard or perform other actions on successful login
-        navigate('/')
-    })
+    const SetData = (e) => {
+        e.preventDefault()
     
-  };
+        fetch('/login', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email, password
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            
+            navigate('/dashboard')
+        })
+    }
 
-  return (
-    <div className="login-box" >
-      <div className="login-logo">
-        {/* <a href="#"><b>Your</b>App</a> */}
-      </div>
-      <div className="card">
-        <div className="card-body login-card-body">
-          <p className="login-box-msg">Sign in here</p>
 
-          <form onSubmit={handleLogin}>
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="email"
-                value={email}
-                onChange={(e) => setemail(e.target.value)}
-                required
-              />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-user"></span>
-                </div>
-              </div>
-            </div>
-            <div className="input-group mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-lock"></span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-8">
-                <div className="icheck-primary">
-                  <input type="checkbox" id="remember" />
-                  <label htmlFor="remember">Remember Me</label>
-                </div>
-              </div>
-              <div className="col-4">
-                <button type="submit" className="btn btn-primary btn-block">Sign In</button>
-              </div>
-            </div>
-          </form>
 
-          <p className="mb-1">
-            {/* <a href="#">Forgot password</a> */}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="container mt-5">
+                        <div className='row'>
 
-export default Login;
+                            <div className="col-sm-6 offset-md-3 offset-sm-1 ">
+                                <form method="POST">
+
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input type="email" className="form-control" id="email" name="email" value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            placeholder="Enter your Email" />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password" className="form-control" id="password" name="password" value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                           
+                                            placeholder="Enter your Password" />
+                                    </div>
+
+                                    <NavLink to='/register'>Didn't Register, then register here!</NavLink><br /><br />
+                                    <button type="submit" className="btn btn-primary" id='login' name='login' onClick={SetData} >Login</button>
+
+                                </form>
+                            </div>
+
+                        </div>
+
+                    </div>
+    )
+}
+
+export default Login
--- app/src/Components/Register.js
+++ app/src/Components/Register.js
@@ -1,8 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
+
 
 const Register = () => {
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    firstname:"", lastname:"", email:"", password:"", confirmPassword:""
+  })
+
+  const handleInputs = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({ ...user, [name]: value })
+  }
+
+  const PostData = (e, firstname, lastname, email, password, confirmPassword) => {
+    e.preventDefault()
+
+    fetch('/register', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            firstname, lastname, email, password, confirmPassword
+        })
+    })
+    .then(res => res.json())
+        
+    .then(data => {
+        console.log(data)
+            navigate('/')
+    })
+}
+    
+    
+
   return (
-    <h1>Register</h1>
+    <div className="container mt-5">
+    <div className='row'>
+        <div className='col-12 col-md-7 col-sm-6 ' >
+            <h1 className='mt-5'>
+                Welcome!
+            </h1>
+        </div>
+
+        <div className="col-12 col-md-5 col-sm-6">
+            <form method='POST'>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" id="email" name='email' placeholder="Enter your Email" value={user.email} onChange={handleInputs}/>
+                </div>
+
+              
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' placeholder="Enter your Password" value={user.password} onChange={handleInputs}/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="cpassword">Confirm password</label>
+                    <input type="password" className="form-control" id="confirmPassword" name='confirmPassword' placeholder="Confirm password" value={user.confirmPassword} onChange={handleInputs}/>
+                </div>
+                <NavLink to='/login'>Already Registered, then Login here!</NavLink><br /><br />
+                <button type="submit" className="btn btn-primary" id='register' name='register' onClick={PostData}>Register</button>
+
+            </form>
+        </div>
+
+    </div>
+
+</div>
   )
 }
 
