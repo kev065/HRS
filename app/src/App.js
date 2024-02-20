287ebb9e17360fb0d60a146a8236b3a1b64ce59d
KELVIN MURITHI <kelvinmurithi@live.com>
2024-02-20 14:06:10 +0300

    Refactor routing in App.js
    Replace anchor tags with button elements for non-navigational actions.
    
    Add a redirect from the root path to the home page.
    
    Ensure Header, SideNav, and Footer components are always rendered on the /home route.

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; 
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -7,9 +7,9 @@ import Register from './components/Register';
 import Footer from './components/Footer'
 import Header from './components/Header'
 import SideNav from './components/SideNav';
+
 function App() {
   const [isRegisterFormActive, setIsRegisterFormActive] = useState(true);
-  
 
   const toggleForm = () => {
     setIsRegisterFormActive(!isRegisterFormActive);
@@ -17,15 +17,15 @@ function App() {
 
   const redirectToLogin = () => {
     console.log("Redirecting to login")
-    
   };
+
   return (
     <Router>
       <div className="App">
+        <Header />
+        <SideNav />
         <Routes>
-          <Route element={<Header />} />
-          <Route element={<SideNav />} />
-          <Route element={<Footer />} />
+          <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route 
             path="/login"
@@ -36,6 +36,7 @@ function App() {
             element={<Register redirectToLogin={redirectToLogin} isRegisterFormActive={isRegisterFormActive} toggleForm={toggleForm} />}
           />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
