db3040e4ec103f3f3c7b50df7b2e39c55d8d953e
KELVIN MURITHI <kelvinmurithi@live.com>
2024-02-21 11:22:46 +0300

    feat: Add AddEmployeeForm route to App.js
    This commit introduces a new route '/add_employee' to the App.js file which renders the AddEmployeeForm component.
    
    This change allows HR personnel to navigate to the 'Add Employee page'.
    
    Can be accessed via this URL: http://localhost:3000/add_employee

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import MainPage from './components/MainPage';
 import Login from './components/Login';
 import DashBoardHr from './components/hrUI/DashBoardHr';
 import Training from './components/hrUI/Training';
+import AddEmployeeForm from './components/hrUI/AddEmployee';
 
 function App() {
   const [trainings, setTrainings] = useState([]);
@@ -18,6 +19,7 @@ function App() {
           <Route path="/login" element={<Login/>} />    
           <Route path="/hr_dashboard" element={<DashBoardHr />}/>
           <Route path="/training_page" element={<Training trainings={trainings} setTrainings={setTrainings}/>}/>
+          <Route path="/add_employee" element={<AddEmployeeForm />}/>
 
         </Routes>
       </div>
