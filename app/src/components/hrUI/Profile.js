06b0087ea6263df3e3d9dc74cbdaeaa8d55760bd
anisamuema <anisa.muema.school@gmail.com>
2024-02-22 11:35:50 +0300

    feat: view hr profile complete

--- app/src/components/hrUI/Profile.js
+++ app/src/components/hrUI/Profile.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
-
+import './profile.css'
+import SideNavHr from './SideNavHr';
 
 const Profile = () => {
 
@@ -32,32 +33,6 @@ const Profile = () => {
     }
   };
 
-  const updateProfile = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:5555/hrProfiles', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userProfile),
-      });
-
-      if (response.ok) {
-        console.log('User profile updated successfully');
-      } else {
-        console.error('Failed to update user profile');
-      }
-    } catch (error) {
-      console.error('Error updating user profile:', error);
-    }
-  };
-
-  const handleChange = (field, value) => {
-    setUserProfile((prevProfile) => ({
-      ...prevProfile,
-      [field]: value,
-    }));
-  };
 
   useEffect(() => {
     fetchUserProfile();
@@ -65,83 +40,91 @@ const Profile = () => {
 
   return (
     <div>
-      <h1>User Profile</h1>
-      <div>
-        <label>Date of Birth:</label>
-        <input
-          type="date"
-          value={userProfile.dateOfBirth}
-          onChange={(e) => handleChange('dateOfBirth', e.target.value)}
-        />
+      <section className="section about-section gray-bg" id="about">
+      <div className="container">
+        <div className="row align-items-center flex-row-reverse">
+          <div className="col-lg-6">
+            <div className="about-text go-to">
+              <h3 className="dark-color">About Me</h3>
+              <h6 className="theme-color lead">A Lead UX &amp; UI designer based in Canada</h6>
+              <p>I <mark>design and develop</mark> services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface and meaningful interactions.</p>
+              <div className="row about-list">
+                <div className="col-md-6">
+                  <div className="media">
+                    <label>Birthday</label>
+                    <p>4th april 1998</p>
+                  </div>
+                  <div className="media">
+                    <label>Age</label>
+                    <p>22 Yr</p>
+                  </div>
+                  <div className="media">
+                    <label>Name</label>
+                    <p>Jane Njiru</p>
+                  </div>
+                  <div className="media">
+                    <label>Joined Date</label>
+                    <p>15th February 2024</p>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="media">
+                    <label>E-mail</label>
+                    <p>info@domain.com</p>
+                  </div>
+                  <div className="media">
+                    <label>Phone</label>
+                    <p>820-885-3321</p>
+                  </div>
+                  <div className="media">
+                    <label>Skype</label>
+                    <p>skype.0404</p>
+                  </div>
+                  <div className="media">
+                    <label>Mantra</label>
+                    <p>Live and Let live</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="about-avatar">
+              <img src="https://bootdey.com/img/Content/avatar/avatar7.png" title="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="counter">
+          <div className="row">
+            <div className="col-6 col-lg-3">
+              <div className="count-data text-center">
+                <h6 className="count h2" data-to="500" data-speed="500">500</h6>
+                <p className="m-0px font-w-600">Happy Clients</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-3">
+              <div className="count-data text-center">
+                <h6 className="count h2" data-to="150" data-speed="150">150</h6>
+                <p className="m-0px font-w-600">Project Completed</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-3">
+              <div className="count-data text-center">
+                <h6 className="count h2" data-to="850" data-speed="850">850</h6>
+                <p className="m-0px font-w-600">Photo Capture</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-3">
+              <div className="count-data text-center">
+                <h6 className="count h2" data-to="190" data-speed="190">190</h6>
+                <p className="m-0px font-w-600">Telephonic Talk</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={userProfile.firstName}
-          onChange={(e) => handleChange('firstName', e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={userProfile.lastName}
-          onChange={(e) => handleChange('lastName', e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label>Mantra:</label>
-        <textarea
-          value={userProfile.mantra}
-          onChange={(e) => handleChange('mantra', e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label>Phone Contact:</label>
-        <input
-          type="text"
-          value={userProfile.phoneContact}
-          onChange={(e) => handleChange('phoneContact', e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label>Profile Image URL:</label>
-        <input
-          type="text"
-          value={userProfile.profileImage}
-          onChange={(e) => handleChange('profileImage', e.target.value)}
-        />
-        <img
-          src={userProfile.profileImage}
-          alt="Profile"
-          style={{ width: '150px', height: '150px' }}
-        />
-      </div>
-
-      <div>
-        <label>Date Joined:</label>
-        <input
-          type="date"
-          value={userProfile.dateJoined}
-          onChange={(e) => handleChange('dateJoined', e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label>Title:</label>
-        <input
-          type="text"
-          value={userProfile.title}
-          onChange={(e) => handleChange('title', e.target.value)}
-        />
-      </div>
-      <button onClick={updateProfile}>Update Profile</button>
+    </section>
+    <SideNavHr />
     </div>
   )
 }
