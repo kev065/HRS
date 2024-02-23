cc9c325b690eb6cb053822afd9fc2af7b301892c
KELVIN MURITHI <kelvinmurithi@live.com>
2024-02-23 12:32:45 +0300

    feat: import and implement css styling for viewing goals component

--- app/src/components/employeeUI/ViewGoalsEmployee.js
+++ app/src/components/employeeUI/ViewGoalsEmployee.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { retrieve } from "../Encryption";
+import './ViewGoalsEmployee.css';
 
 const Goals = () => {
     const [goals, setGoals] = useState([]);
@@ -20,18 +21,18 @@ const Goals = () => {
     }, []);
 
     return (
-        <div>
-            <h1>My Goals</h1>
-            <div>
-                {goals.map((goal, index) => (
-                    <div key={index}>
-                        <h2>{index + 1}. {goal.name}</h2>
-                        <p>{goal.description}</p>
-                    </div>
-                ))}
-            </div>
+        <div className="container">
+          <h1>My Goals</h1>
+          <div>
+            {goals.map((goal, index) => (
+              <div key={index} className="goal">
+                <h2>{index + 1}. {goal.name}</h2>
+                <p>{goal.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
-    );
-};
-
+      );
+    };
+    
 export default Goals;
