6917cd19f992d985ce493de9a2922daabcc3492d
Allan Njoroge <allcreativewrites@gmail.com>
2024-02-24 14:35:32 +0300

    Added ViewPayslipEmployee route

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import './App.css';
-import MainPage from './components/MainPage';
-import Login from './components/Login';
-import DashBoardHr from './components/hrUI/DashBoardHr';
-import DashBoardEmployee from './components/employeeUI/DashBoardEmployee';
-import Trainings from './components/hrUI/Trainings';
-import AddEmployeeForm from './components/hrUI/AddEmployee';
-import UpdateTrainings from './components/hrUI/UpdateTrainings';
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import MainPage from "./components/MainPage";
+import Login from "./components/Login";
+import DashBoardHr from "./components/hrUI/DashBoardHr";
+import DashBoardEmployee from "./components/employeeUI/DashBoardEmployee";
+import Trainings from "./components/hrUI/Trainings";
+import AddEmployeeForm from "./components/hrUI/AddEmployee";
+import UpdateTrainings from "./components/hrUI/UpdateTrainings";
 import ViewEmployees from "./components/hrUI/ViewEmployees";
 import VerifyDocuments from "./components/hrUI/VerifyDocuments";
 import EducationDocumentUpload from "./components/employeeUI/EducationDocumentUpload";
@@ -20,15 +20,16 @@ import UpdateLeave from "./components/employeeUI/UpdateLeave";
 import EmployeeProfileForm from "./components/employeeUI/EmployeeProfileForm";
 import EmployeeProfile from "./components/employeeUI/EmployeeProfile";
 import EditProfileForm from "./components/employeeUI/EditProfileForm";
-import Session from './components/hrUI/Session';
-import Profile from './components/hrUI/Profile'
-import UpdateProfile from './components/hrUI/UpdateProfile';
-import LeaveApprovalForm from './components/hrUI/PendingLeavesHr';
-import ManagerPendingLeaves from './components/managerUI/LeaveApproval_Manager';
-import DashBoardManager from './components/managerUI/DashBoardManager';
-import ManagerApprovedLeaves from './components/managerUI/ManagerApprovedLeaves';
-import Experience from './components/employeeUI/AddExperience';
-import Goals from './components/employeeUI/ViewGoalsEmployee';
+import Session from "./components/hrUI/Session";
+import Profile from "./components/hrUI/Profile";
+import UpdateProfile from "./components/hrUI/UpdateProfile";
+import LeaveApprovalForm from "./components/hrUI/PendingLeavesHr";
+import ManagerPendingLeaves from "./components/managerUI/LeaveApproval_Manager";
+import DashBoardManager from "./components/managerUI/DashBoardManager";
+import ManagerApprovedLeaves from "./components/managerUI/ManagerApprovedLeaves";
+import Experience from "./components/employeeUI/AddExperience";
+import Goals from "./components/employeeUI/ViewGoalsEmployee";
+import ViewPayslipEmployee from "./components/employeeUI/ViewPayslipEmployee";
 
 function App() {
   const [trainings, setTrainings] = useState([]);
@@ -38,39 +39,74 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/session" element={<Session />}/>
+        <Route path="/session" element={<Session />} />
         <Route path="/employee" element={<DashBoardEmployee />}>
-       <Route path="/employee/view_education/:employeeId" element={<ViewEducation />} />
-       <Route path="/employee/education/document/upload" element={<EducationDocumentUpload />}/>
-       <Route path="/employee/update_document/:id" element={<UpdateDocuments />} />
-       <Route path="/employee/view_leaves/:id" element={<ViewLeaves />} />
-       <Route path="/employee/update_leave/:id" element={<UpdateLeave />} />
-       <Route path="/employee/profile" element={<EmployeeProfile/>} />
-        <Route path="/employee/profile/create" element={<EmployeeProfileForm />} />
-        <Route path="/employee/profile/edit" element={<EditProfileForm />} />
-        <Route path="/employee/view_documents/:id" element={<ViewDocuments />} />
-        <Route path="/employee/update_education/:id" element={<UpdateEducation />} />
-        <Route path="/employee/goals" element={<Goals />}/>
-        <Route path="/employee/experience" element={<Experience />}/>
-       </Route>
-        <Route path="/hr" element={<DashBoardHr />} >
-        <Route path="/hr/training_page" element={<Trainings trainings={trainings} setTrainings={setTrainings} />  } />
-        <Route path="/hr/add_employee" element={<AddEmployeeForm />} />      
-        <Route path="/hr/update_trainings/:id" element={<UpdateTrainings />} />
-        <Route path="/hr/view_employees" element={<ViewEmployees />} />
-        <Route path="/hr/verify_documents/:employeeId"  element={<VerifyDocuments />} />
-        <Route path="/hr/hr_pending_leaves" element={<LeaveApprovalForm />}/>
-        <Route path="/hr/hr_update_profile" element={<UpdateProfile />}/>
-        <Route path="/hr/hr_profile" element={<Profile />}/>
-        </Route>   
-
-        <Route path="/manager" element={<DashBoardManager />} >
-        <Route path="/manager/manager_pending_leaves" element={<ManagerPendingLeaves />}/>
-        <Route path="/manager/manager_approved_leaves" element={<ManagerApprovedLeaves />}/>
+          <Route
+            path="/employee/view_education/:employeeId"
+            element={<ViewEducation />}
+          />
+          <Route
+            path="/employee/education/document/upload"
+            element={<EducationDocumentUpload />}
+          />
+          <Route
+            path="/employee/update_document/:id"
+            element={<UpdateDocuments />}
+          />
+          <Route path="/employee/view_leaves/:id" element={<ViewLeaves />} />
+          <Route path="/employee/update_leave/:id" element={<UpdateLeave />} />
+          <Route path="/employee/profile" element={<EmployeeProfile />} />
+          <Route
+            path="/employee/profile/create"
+            element={<EmployeeProfileForm />}
+          />
+          <Route path="/employee/profile/edit" element={<EditProfileForm />} />
+          <Route
+            path="/employee/view_documents/:id"
+            element={<ViewDocuments />}
+          />
+          <Route
+            path="/employee/update_education/:id"
+            element={<UpdateEducation />}
+          />
+          <Route path="/employee/goals" element={<Goals />} />
+          <Route path="/employee/experience" element={<Experience />} />
+          <Route path="/employee/payslip" element={<ViewPayslipEmployee />} />
         </Route>
-             
-        </Routes>
-      </div>
+        <Route path="/hr" element={<DashBoardHr />}>
+          <Route
+            path="/hr/training_page"
+            element={
+              <Trainings trainings={trainings} setTrainings={setTrainings} />
+            }
+          />
+          <Route path="/hr/add_employee" element={<AddEmployeeForm />} />
+          <Route
+            path="/hr/update_trainings/:id"
+            element={<UpdateTrainings />}
+          />
+          <Route path="/hr/view_employees" element={<ViewEmployees />} />
+          <Route
+            path="/hr/verify_documents/:employeeId"
+            element={<VerifyDocuments />}
+          />
+          <Route path="/hr/hr_pending_leaves" element={<LeaveApprovalForm />} />
+          <Route path="/hr/hr_update_profile" element={<UpdateProfile />} />
+          <Route path="/hr/hr_profile" element={<Profile />} />
+        </Route>
+
+        <Route path="/manager" element={<DashBoardManager />}>
+          <Route
+            path="/manager/manager_pending_leaves"
+            element={<ManagerPendingLeaves />}
+          />
+          <Route
+            path="/manager/manager_approved_leaves"
+            element={<ManagerApprovedLeaves />}
+          />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
